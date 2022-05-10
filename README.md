fd51d35e18004e8d07e7b47c9b83429f74f3c8c5
dogmom3 <blair.seivers@yahoo.com>
2022-05-09 20:19:19 -0400

    add delete functionality

---
+++ README.md
@@ -0,0 +1,15 @@
+# Note Taker App
+
+## Description
+My motivation in creating this Note Taker App is to expand my knowledge with using Express and connecting front end and back end code.
+I built this project to help organize notes.
+This will make it easy to see what is on your to do list and delete as completed.
+I learned how to use Express and Heroku to help further my coding abilities.
+
+*Link to deployed app: https://note-taker-blair.herokuapp.com/notes
+
+![App Image](./public/assets/images/note-taker.png)
+
+## Credits
+
+created by Blair Seivers :)
---
+++ server.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+// const db = require('./db/db.json');
+// const readWrite = require('./db/read-write');
+const PORT = process.env.PORT || 3002;
+// const uuid = require('./helpers/uuid');
+const util = require('util');
+const promiseReadFile = util.promisify(fs.readFile);
+const promiseWriteFile = util.promisify(fs.writeFile);
+var uniqid = require('uniqid'); 
+console.log(uniqid()); // -> 4n5pxq24kpiob12og9
+
+//handles the post request
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+//get the notes.html file
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+//function to read file
+function readDb() {
+  return promiseReadFile('./db/db.json', 'utf8').then(function (data) {
+      console.log(data)
+      return (JSON.parse(data))
+  })
+}
+// function to write file. Array of objects to db.json
+function writeDb() {
+  return promiseWriteFile('./db.db.json').then(function (data) {
+      console.log(data)
+      return (JSON.parse(data))
+  })
+}
+
+// adding in notes ?
+app.get('/api/notes', (req, res) => {
+  // readWrite.
+  readDb().then(function (data) {
+    res.json(data)
+  })
+});
+
+// // POST request to add a note
+app.post('/api/notes', (req, res) => {
+  // readWrite.
+  readDb().then(function (data) {
+    const note = req.body;
+    console.log('note', note)
+    note.id = uniqid()
+    console.log('updatedNote', note)
+    data.push(note);
+    console.log('db', data)
+    fs.writeFile('./db/db.json', JSON.stringify(data), function () {
+      res.json(data)
+    })
+  })
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  // readWrite.
+  readDb().then(function (data) {
+    // const note = req.body;
+    // data.push(note);
+    console.log('flag', req.params.id),
+    console.log('db', data)
+    const newNote = data.filter(element => element.id !== req.params.id);
+    console.log('here')
+console.log('new flag', newNote)
+
+
+
+    fs.writeFile('./db/db.json', JSON.stringify(newNote), function () {
+      res.json(newNote)
+    })
+  })
+});
+
+// get the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.listen(PORT, () =>
+  console.log(`Note Taker App is running at http://localhost:${PORT} 🚀`)
+);
+
+module.exports = {
+  readDb: readDb,
+  writeDb: writeDb
+}
+
+
